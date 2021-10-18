f5cf2e7561ae4943b8c7645ab212cca9b7c98654
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-10-18 18:10:20 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2020 brainlife.io
+#
+# This file is a MNE python-based brainlife.io App
+#
+# Author: Guiomar Niso
+# Indiana University
+
+# Required libraries
+# pip install mne-bids coloredlogs tqdm pandas scikit-learn json_tricks fire
+
+# set up environment
+#import mne-study-template
+import os
+import json
+import mne
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Current path
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# Load brainlife config.json
+with open(__location__+'/config.json') as config_json:
+    config = json.load(config_json)
+
+# == LOAD DATA ==
+# FIF
+fname = config['fif']
+raw = mne.io.read_raw_fif(fname)
+
+# CTF
+# fname = config['ctf']
+# raw = mne.io.read_raw_ctf(fname)
+
+# == GET CONFIG VALUES ==
+duration = config['duration']
+
+# == EPOCH DATA ==
+#Make epohs of equal length
+epochs = mne.make_fixed_length_epochs(raw, duration=duration, preload=False)
+
+# == SAVE FILE ==
+epochs.save(os.path.join('out_dir','meg.fif'))
+
+
+
